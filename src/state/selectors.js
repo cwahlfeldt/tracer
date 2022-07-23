814210a4c801d0c6d4c261eadbea5edca70ea7a2
Chris Wahlfeldt <chrisw@pixotech.com>
2022-07-22 22:47:47 -0500

    wip

--- src/state/selectors.js
+++ src/state/selectors.js
@@ -1,13 +1,17 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { convertHexToPixel } from '../lib/hex.js';
+import { createSelector } from '@reduxjs/toolkit'
+import { convertHexToPixel } from '../lib/hex.js'
+
+export const findPlayer = (board) => {
+    return board.filter((tile) => 'player' in tile.props)[0]
+}
 
 export const selectPlayer = createSelector(
     (state) => state.board,
     (board) => {
-        const tile = board.filter((tile) => 'player' in tile.props)[0];
+        const tile = findPlayer(board)
         return {
             ...tile.props.player,
             location: convertHexToPixel(tile.hex),
-        };
+        }
     }
-);
+)
