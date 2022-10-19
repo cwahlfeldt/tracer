4d768905443cacdd56d3b86274ea0a25ad25865f
chris <chrisw@pixotech.com>
2022-10-18 20:42:16 -0500

    delete old stuff

--- src/pieces/enemyPiece.js
+++
@@ -1,6 +0,0 @@
-import {drawCircle} from "../lib/canvasUtilities.js";
-import { CTX } from "../lib/consts.js";
-
-export default function enemyPiece(location) {
-    drawCircle(CTX, location.x, location.y, 'red')
-}
--- src/pieces/playerPiece.js
+++
@@ -1,6 +0,0 @@
-import {drawCircle} from "../lib/canvasUtilities.js";
-import { CTX } from "../lib/consts.js";
-
-export default function playerPiece(location) {
-    drawCircle(CTX, location.x, location.y, 'cyan')
-}
